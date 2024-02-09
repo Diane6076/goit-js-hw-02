a982ba78b5b8696e63d92f9e0713ea2b3306375c
diana <dskripka005@gmail.com>
2024-02-09 20:08:57 +0200

    fixfunc

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
 // Завдання 1
-
-// const sliderInput = document.querySelector('.slider__input');
-//   const sliderImage = document.querySelector('.slider__image');
-
-//   const debounceResize = _.debounce(() => {
-//     const value = sliderInput.value;
-//     const newSize = value * 3; 
-//     sliderImage.style.width = newSize + 'px';
-//   }, 300);
-//   sliderInput.addEventListener('input', debounceResize);
-
 const slidInput = document.querySelector('.slider__input');
 const slidImg = document.querySelector('.slider__image');
 
-slidInput.addEventListener('input', _.debounce((slidImgfunct), 300))
+slidInput.addEventListener("slidInput", _.debounce((changer), 200))
 
-function slidImgfunct() {
-    slidImg.style.rotate = `${slidInput.value}deg`;
+function changer() {
+    slidImg.style.width = `${slidInput.value * 5}px`;
+    slidImg.style.height = `${slidInput.value * 5}px`;
 }
--- js/main2.js
+++ js/main2.js
@@ -1,17 +1,4 @@
 // Завдання 2
-// const box = document.getElementById('box');
-
-//   const debounceMove = _.debounce((event) => {
-//     const mouseX = event.clientX;
-//     const mouseY = event.clientY;
-//     const translateX = mouseX - box.offsetWidth / 2;
-//     const translateY = mouseY - box.offsetHeight / 2;
-
-//     box.style.transform = `translate(${translateX}px, ${translateY}px)`;
-//   }, 100); 
-//   document.addEventListener('mousemove', debounceMove);
-
-
 
 const box = document.querySelector('#box');
 const boxAll = document.querySelector('#boxall')
@@ -19,5 +6,4 @@ const boxAll = document.querySelector('#boxall')
 boxAll.addEventListener('mouse',  _.debounce((event) => {
  box.style.left = `${event.offsetX}px`;
  box.style.top = `${event.offsetY}px`;
-  
-}));
+}, 100));
