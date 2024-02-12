efd8f922ad8d8e0726ad9dfc58e3f1d94bcaf64f
diana <dskripka005@gmail.com>
2024-02-12 14:37:37 +0200

    taskonefix

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 const slidInput = document.querySelector('.slider__input');
 const slidImg = document.querySelector('.slider__image');
 
-slidInput.addEventListener("slidInput", _.debounce((changer), 200))
+slidInput.addEventListener('slidInput', _.debounce(changer, 500))
+// slidInput.addEventListener("slidInput", _.debounce(changer, 200) )
 
-function changer() {
-    slidImg.style.width = `${slidInput.value * 5}px`;
-    slidImg.style.height = `${slidInput.value * 5}px`;
+function changer(slidInput) {
+    slidImg.style.width = `${slidInput.target.value * 5}px`;
+    slidImg.style.height = `${slidInput.target.value * 5}px`;
 }
